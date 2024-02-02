0bb45738bf1100539f05cfbddcb1330315ab75c5
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-02-02 15:37:21 +0600

    добавил фунцию для отображение данных о стране

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,71 @@
 import React, { useEffect, useState } from 'react';
 
-// Определение типа для страны
-type Country = {
-  alpha3Code: string;
-  name: string;
-};
 
 const App: React.FC = () => {
-  const url = 'https://restcountries.com/v2/all?fields=alpha3Code,name';
+  const countriesListUrl = 'https://restcountries.com/v2/all?fields=alpha3Code,name';
+  const countryInfoBaseUrl = 'https://restcountries.com/v2/alpha/';
 
   const [countries, setCountries] = useState<Country[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
 
-  const fetchCountries = async () => {
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(countriesListUrl);
+        const data: Country[] = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
+   void fetchCountries();
+  }, []);
+
+  const handleCountryClick = async (alpha3Code: string) => {
     try {
-      const response = await fetch(url);
-      const data: Country[] = await response.json();
-      setCountries(data);
+      const response = await fetch(`${countryInfoBaseUrl}${alpha3Code}`);
+      const data: Country = await response.json();
+      setSelectedCountry(data);
     } catch (error) {
-      console.error('Error fetching countries:', error);
+      console.error('Error fetching country info:', error);
     }
   };
 
-  useEffect(() => {
-    void fetchCountries();
-  }, []);
-
   return (
     <div>
-      <ul>
-        {countries.map((country) => (
-          <li key={country.alpha3Code}>
-            {country.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className='header'>
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Страны</a>
+          </div>
+        </nav>
+      </div>
+      <div className='main' style={{display: "flex"}}>
+        <div className="box" style={{width: '400px',}}>
+          <ul className="list-group">
+            {countries.map((country) => (
+              <li className="list-group-item" key={country.alpha3Code}
+                  onClick={() => handleCountryClick(country.alpha3Code)}>
+                {country.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='boxes ms-3'>
+            <h2>Информация о стране</h2>
+            {selectedCountry && (
+              <div>
+                <h3>{selectedCountry.name }</h3>
+                <p>Столица: {selectedCountry.capital}</p>
+                <p>Население: {selectedCountry.population}</p>
+                <p>Площадь: {selectedCountry.area} км²</p>
+                <p>Граничит с: {selectedCountry.borders.join(', ')}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
   );
 };
 
---
+++ src/type.d.ts
@@ -0,0 +1,8 @@
+interface  Country {
+  alpha3Code: string;
+  name: string;
+  capital: string;
+  population: number;
+  area: number;
+  borders: string[];
+}
