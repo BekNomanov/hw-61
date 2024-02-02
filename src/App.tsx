c532e0ae5ca8593b45359d419344a7fa4738b0dd
Bekbolot Nomanov <nomanovbekbolot7@gmail.com>
2024-02-02 14:18:16 +0600

    initials commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+// Определение типа для страны
+type Country = {
+  alpha3Code: string;
+  name: string;
+};
+
+const App: React.FC = () => {
+  const url = 'https://restcountries.com/v2/all?fields=alpha3Code,name';
+
+  const [countries, setCountries] = useState<Country[]>([]);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(url);
+      const data: Country[] = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  };
+
+  useEffect(() => {
+   void fetchCountries();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.alpha3Code}>
+            {country.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
